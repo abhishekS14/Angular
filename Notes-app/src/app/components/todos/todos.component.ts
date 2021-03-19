4a0546db35b872a6c6b9b70580d7d6616014ddfd
abhishekS14 <abhisheksupsande7@gmail.com>
2021-03-19 17:32:15 +0530

    Updated fonts

---
+++ Notes-app/src/app/components/todos/todos.component.ts
@@ -0,0 +1,40 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from "./../../models/Todo"; 
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos: Todo[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = []
+  }
+  inputTodo:string = "";
+
+  toggleDone (id:number) {
+    this.todos.map((v , i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+  deleteTodo(id:number) {
+    this.todos = this.todos.filter((v,i) => i !== id);
+  }
+
+  addTodo() {
+    this.todos.push({
+      content:this.inputTodo,
+      completed:false
+    });
+
+    this.inputTodo ="";
+  }
+
+}
